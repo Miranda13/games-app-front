f2f84843c7dad8f533f7a24930d14deef2ee65dc
Anamaria Miranda <anny13miranda@gmail.com>
2023-10-20 09:48:22 -0500

    Config tailwind styles

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Core from './app/core'
+import './index.scss'
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Core/>
+  </React.StrictMode>,
+)
