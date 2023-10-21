8d52350b5a7cff1c10329b6a7d3b366078a4f7b3
Anamaria Miranda <anny13miranda@gmail.com>
2023-10-21 16:22:25 -0500

    Added home view and its components

---
+++ src/app/views/Home/components/cardsGame/index.tsx
@@ -0,0 +1,30 @@
+import { IGame } from '../../../../../models/games.model'
+import CardGame from '../cardGame'
+
+type CardsGameProps = {
+  data: [],
+  setActionDelete: (change: boolean) => void,
+  setActionUpdate: (change: boolean) => void
+}
+
+const CardsGame = (props: CardsGameProps) => {
+
+  const { data, setActionDelete, setActionUpdate } = props
+
+  return (
+    <section className='flex flex-wrap w-100 justify-center m-6'>
+      {
+        data.length > 0 ?
+        data.map((game: IGame) => 
+          <CardGame key={`game${game.game_id}`} 
+            setActionUpdate={setActionUpdate}
+            setActionDelete={setActionDelete}
+            data={game} />
+        ) :
+        <p> No hay juegos registrados </p>
+      }
+    </section>
+  )
+}
+
+export default CardsGame
